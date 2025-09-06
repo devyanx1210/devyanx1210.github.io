8875a1340bffb73d44660c2228ea271bed4d134c
Noel Christian L. Soberano <christianclegane@gmail.com>
2025-09-06 23:45:19 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   base: "/", // root
-//   plugins: [react()],
-// });
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: './', // correct for root GitHub Pages deployment
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-  },
-});
+})
