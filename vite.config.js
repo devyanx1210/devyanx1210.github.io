70867bfc4a78fcc8b251a71e65af0b1c207fa58e
devyanx1210 <csoberano1210@gmail.com>
2025-09-08 04:49:28 +0800

    edit vit.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,8 @@ export default defineConfig({
   base: "/devyanx1210.github.io/",
   plugins: [react()],
   server: {
-    host: "localhost", // or "0.0.0.0" if you want access from other devices
-    port: 5173, // default port
-    strictPort: true, // if true, it won’t pick a random port
-    open: true, // auto-open browser
-    hmr: {
-      protocol: "ws", // WebSocket protocol
-      host: "localhost", // match your dev host
-      port: 5173, // same as server.port
-    },
     watch: {
-      usePolling: true, // useful for WSL, Docker, or network drives
+      usePolling: true,
     },
   },
 });
